3e33ac450cc0851317deb9367027b63d7fac734d
nischaybanga <banga.nischay2@gmail.com>
2022-06-01 16:04:43 +0530

    line

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <h3 className="text-class">Let's calculate <b>distance</b> from Google maps</h3>
     </div>
   );
 }
